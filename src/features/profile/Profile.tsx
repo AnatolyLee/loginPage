7d4b69862c5c85aa62d4856e3f7163144151466b
AnatolyLee <toliklee007@gmail.com>
2024-11-25 21:52:17 +0600

    проверка

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+
 import ProfileHeader from "./ProfileHeader";
 import ProfileUser from "./ProfileUser";
 import ProfileMenu from "./ProfileMenu";
+
 
 const Profile: React.FC = () => {
   return (
