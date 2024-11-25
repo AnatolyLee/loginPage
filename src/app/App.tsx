f9dcbd0261de7684ca90ba72c4ae0cb45c0962e4
AnatolyLee <toliklee007@gmail.com>
2024-11-25 21:46:55 +0600

    контрольный

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Profile from "../pages/profile/Profile";
+import Profile from "../features/profile/Profile";
 
 
 const App: React.FC = () => {
--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ProfileHeader from "./ProfileHeader";
 import ProfileUser from "./ProfileUser";
 import ProfileMenu from "./ProfileMenu";
-import ProfileFooter from "./ProfileFooter";
-import "./Profile.css";
 
 const Profile: React.FC = () => {
   return (
@@ -11,8 +9,6 @@ const Profile: React.FC = () => {
       <ProfileHeader />
       <ProfileUser />
       <ProfileMenu />
-      <button className="logout-button">Выйти</button>
-      <ProfileFooter />
     </div>
   );
 };
